0888922c27211d6c2e8ecf557fb6680317c7cda9
HendiTI21K1 <hendisupriadi123@gmail.com>
2024-09-18 19:27:28 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "login_quiz2_hendisupriadi"
+include("login_quiz2_hendisupriadi:app")login_quiz2_hendisupriadi"
+ 
